7792f557b1995b4cf9de057b91cf373680bbd7a2
chinenmiki <awasefall@yahoo.co.jp>
2026-06-13 19:25:30 +0900

    Add Sorcery authentication and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   # Sorceryの認証機能をこのモデルで有効にするための宣言
 
-  validates :user_name, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true, length: { maximum: 255 }
   # ユーザー名の存在チェックと重複禁止
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
