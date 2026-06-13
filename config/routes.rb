1dd07129af5ef30e8e54f5a30454bfaaecd5e2c5
chinenmiki <awasefall@yahoo.co.jp>
2026-06-13 21:43:15 +0900

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # ルートページ（トップページ）
   root 'static_pages#top'
 
-  resources :users, only: [:new, :create]
+  # ログイン関連
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
   # ユーザー登録
+  resources :users, only: [:new, :create]
+
 end
