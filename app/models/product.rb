0f9a522b885528f189aed5d3ab6ae02ba1adcdb0
chinenmiki <awasefall@yahoo.co.jp>
2026-06-18 01:06:30 +0900

    Update: 感想を任意項目に変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :category_id, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true
-  validates :comment, presence: true
+  # validates :comment, presence: true
 end
